2ac85cfb2b02cedbcc28cb3860389a09fcd134f9
victorVaqueroBerra <virtus.mv@gmail.com>
2019-06-20 09:29:38 +0200

    main file.

--- main.py
+++ main.py
@@ -13,13 +13,27 @@ od_obj = Od(LED_LIGHT_SENSOR_PIN, LIGHT_SENSOR_PIN)
 pump_water = Stepper(STP1_PIN, DIR1_PIN)
 pump_algae = Stepper(STP2_PIN, DIR2_PIN)
 
+green_led = machine.Pin(LED_G_PIN, machine.Pin.OUT)
+
+button = Button(BUTTON_PIN)
+
+# blink led 3 times just to check that everything is working so far.
+for i in range(3):
+    green_led.value(1)
+    utime.sleep(1)
+    green_led.value(0)
+    utime.sleep(1)
+
 P, I, D = 1, 1, 1
 
 while True:
     try:
         # Publish
         if accum_time >= PUBLISH_PERIOD_IN_SEC:
+            print('before temperature')
             temp = read_temp(temp_sens)
+            print('after temperature')
+
             od_value = od_obj.get_OD_measurement()
             send_temperature(temp)
             send_od(od_value)
@@ -27,16 +41,16 @@ while True:
             P, I, D = get_value(client_P, P), get_value(client_I, I), get_value(client_D, D)
             pid_object = PID(float(P), float(I), float(D))
             print('after pid object')
-            pid_object.update(temp)
+            pid_object.update(temp - 19)
             pid_action = pid_object.output
             number_steps = map_action_to_steps(pid_action)
             pump_water.rotate_some(1, number_steps)
             print(P, I, D )
 
-            time.sleep(SUBSCRIBE_CHECK_PERIOD_IN_SEC)
-            accum_time += SUBSCRIBE_CHECK_PERIOD_IN_SEC
+        time.sleep(SUBSCRIBE_CHECK_PERIOD_IN_SEC)
+        accum_time += SUBSCRIBE_CHECK_PERIOD_IN_SEC
 
-            print('end while loop')
+
 
     except Exception as e:
         print(str(e))
